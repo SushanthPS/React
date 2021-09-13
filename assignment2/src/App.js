ef17d52614b2541ee9579132cc96d3a8f837c7ed
Sushanth <getsushanthps@gmail.com>
2021-09-13 13:33:27 +0530

    ...

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Todo from "./components/Todo";
 
 const option = 2;
 
@@ -11,7 +12,12 @@ function App() {
             </div>
         );
     } else if (option === 2) {
-        return null;
+        return (
+            <div className="App">
+                <h2>Todo List:</h2>
+                <Todo></Todo>
+            </div>
+        );
     }
 }
 
