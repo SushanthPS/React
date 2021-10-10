625bee01c0540be2128b8ec05f3c374406de2b24
Sushanth PS <35593841+SushanthPS@users.noreply.github.com>
2021-10-11 00:43:41 +0530

    Delete 16-redux-axios directory

--- 16-redux-axios/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import { reducer } from "./reducer.js";
-
-const initialState = {
-    counter: 0,
-    todos: [],
-};
-
-export const store = createStore(reducer, initialState);
